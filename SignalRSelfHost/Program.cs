de515af2174f730070918c0f6e47046d52a5a08a
GRRook <gerbenrook@gmail.com>
2015-10-05 13:56:30 +0200

    New commit

--- SignalRSelfHost/Program.cs
+++ SignalRSelfHost/Program.cs
@@ -45,84 +45,22 @@ namespace SignalRSelfHost
         {
             
             public async void Login(string user)
-            {
-                //Log user object and hubId
-                Console.WriteLine("Login user: {0}", user);
-
-                //Create connection
-                var mongoClient = new MongoClient("mongodb://localhost");
-                var db = mongoClient.GetDatabase("chat");
-                var onlineUsersCollection = db.GetCollection<BsonDocument>("onlineUsers");
-
-                //Get all onlineusers from the database
-                var jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
-                var allOnlineUsers = await onlineUsersCollection.Find("{}").ToListAsync();
-                Clients.Caller.getOnlineUsers(allOnlineUsers.ToJson(jsonWriterSettings));
-                Console.WriteLine("All Online users: " + allOnlineUsers.ToJson(jsonWriterSettings));
-
-                //Parse user object string to BsonDocument
-                MongoDB.Bson.BsonDocument document = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(user);
-                //Insert yourself into database
-                await onlineUsersCollection.InsertOneAsync(document);
-
-                //Get yourself
-                var queryYourSelf = "{hubId : '" + Context.ConnectionId + "'}";
-                var yourSelf = await onlineUsersCollection.Find(queryYourSelf).ToListAsync();
+            {                
+               
                 Clients.Caller.getYourSelf(yourSelf.ToJson(jsonWriterSettings));
-
-
-                //Send yourself as new user to all others out there..
-                Clients.Others.newUser(user);
 
             }
 
             public void SendMessage(string message)
             {
                 Console.WriteLine("Message: {0}", message);
-
-                //Create connection
-                var mongoClient = new MongoClient("mongodb://localhost");
-                var db = mongoClient.GetDatabase("chat");
-                var messageCollection = db.GetCollection<BsonDocument>("message");
-
-                //Parse message object string to BsonDocument
-                MongoDB.Bson.BsonDocument document = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(message);
-
-                //Insert message into database
-                messageCollection.InsertOneAsync(document);
-
-                //connection id receiver
-                string connectionIdTo = null;
                 
-                //parse message so it can be looped 
-                dynamic dynJson = Newtonsoft.Json.JsonConvert.DeserializeObject("[" + message + "]");
-                
-                //Loop trough message object and get the message receiver id
-                foreach (var item in dynJson)
-                {
-                    connectionIdTo = item.toHubId;
-                    connectionIdTo.Trim();                       
-                }
-
-                //Send message object to intended user
-                Clients.Client(connectionIdTo).getMessage(message);
-                
-                //Console write line message pushed to id..
-                Console.WriteLine("Push message to client " + connectionIdTo);
             }
 
             //When user disconnects
             public override Task OnDisconnected(bool stopCalled = true)
             {
-                //Delete user from onlineUsers in database
-                var mongoClient = new MongoClient("mongodb://localhost");
-                var db = mongoClient.GetDatabase("chat");
-                var onlineUsersCollection = db.GetCollection<BsonDocument>("onlineUsers");
-                onlineUsersCollection.DeleteOneAsync("{hubId : '" + Context.ConnectionId + "'}");
-
-                //Delete user from $scope.onlineUsers at clientside
-                Clients.Others.deleteUser(Context.ConnectionId);
-
+                
                 Console.WriteLine("User disconnects " + Context.ConnectionId);
                 return base.OnDisconnected(stopCalled);
             }
