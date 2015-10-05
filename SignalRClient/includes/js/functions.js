2fa38780020eab74cd03a5613d3d5c1f21b8bf47
AdeZwart <andredezwart@gmail.com>
2015-10-05 14:34:02 +0200

    Structured the JS files, added JS CDN refrences, added bootstrap.

---
+++ SignalRClient/includes/js/functions.js
@@ -0,0 +1,9 @@
+﻿//Set the hubs URL for the connection
+$.connection.hub.url = "http://localhost:8080/signalr";
+// Declare a proxy to reference the hub.
+var chatHubConnection = $.connection.myHub;
+
+//Start connection
+$.connection.hub.start().done(function () {
+    chatHubConnection.server.login("hi");
+});
